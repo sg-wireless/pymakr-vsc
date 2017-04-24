9f4509ee8e968d52446bf712e7b4ce732ce67737
Ralph Hogenbirk <ralph@pycom.io>
2017-04-24 16:52:36 +0200

    initial commit

---
+++ src/board/terminal.js
@@ -0,0 +1,93 @@
+'use babel';
+
+let Terminal = require('xterm')
+var vscode = require('vscode');
+
+export default class Term {
+
+    constructor(element,pyboard) {
+      this.term_buffer = ""
+      this.shellprompt = '>>> ';
+      this.element = element
+      this.pyboard = pyboard
+      this.element = element
+      this.onMessage = function(){}
+      this.lastWrite = ""
+      var _this = this
+      this.xterm = new Terminal({
+        cursorBlink: true,
+        rows:11,
+        cols:120
+      });
+
+      this.xterm.on('key',function(key,ev){
+        _this.termKeyPress(_this,key,ev)
+      })
+      this.xterm.open(element);
+    }
+
+    setOnMessageListener(cb){
+      this.onMessage = cb
+    }
+
+    termKeyPress(_this,key,ev){
+      var term = this.xterm
+      if (this.pyboard.connected) {
+        if(ev.keyCode == 67) { // ctrl-c
+          this.copy()
+        }else if(ev.keyCode == 86){ //ctrl-v
+          this.paste(ev)
+        }
+        this.userInput(key)
+      }
+    }
+
+    writeln(mssg){
+      vscode.window.showInformationMessage(mssg);
+      this.xterm.writeln(mssg)
+      this.lastWrite += mssg
+      if(this.lastWrite.length > 20){
+        this.lastWrite = this.lastWrite.substring(1)
+      }
+    }
+
+    write(mssg){
+      vscode.window.showInformationMessage(mssg);
+      this.xterm.write(mssg)
+      this.lastWrite += mssg
+      if(this.lastWrite.length > 20){
+        this.lastWrite = this.lastWrite.substring(1)
+      }
+    }
+
+    writeln_and_prompt(mssg){
+      this.writeln(mssg+"\r\n")
+      this.writePrompt()
+    }
+
+    writePrompt(){
+      this.write(this.shellprompt)
+    }
+
+    enter(){
+      this.write('\r\n')
+    }
+
+    clear(){
+      this.xterm.clear()
+      this.lastWrite = ""
+    }
+
+    userInput(input){
+      this.onMessage(input)
+    }
+
+    paste(){
+      // this.userInput(atom.clipboard.read())
+    }
+
+    copy(ev){
+      // atom.clipboard.write(window.getSelection().toString())
+    }
+
+}
---
+++ src/connections/pytelnet.js
@@ -0,0 +1,130 @@
+'use strict';
+'use babel';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _telnetcli = require('./telnet/telnetcli.js');
+
+var _telnetcli2 = _interopRequireDefault(_telnetcli);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+AYT = '\xff\xf6';
+
+var PyTelnet = function () {
+  function PyTelnet(params) {
+    _classCallCheck(this, PyTelnet);
+
+    this.type = "telnet";
+    this.stream = new _telnetcli2.default('pycomboard');
+    this.connected = false;
+    this.listening = false;
+    this.username_sent = false;
+    this.password_sent = false;
+    this.params = params;
+    this.pingTimer = null;
+    this.receive_buffer = "";
+    this.ayt_pending = false;
+  }
+
+  _createClass(PyTelnet, [{
+    key: 'sendPing',
+    value: function sendPing() {
+      if (this.ayt_pending) {
+        this.ayt_pending = false;
+        return false;
+      }
+      this.ayt_pending = true;
+      this.send(AYT);
+      return true;
+    }
+  }, {
+    key: 'connect',
+    value: function connect(onconnect, onerror, ontimeout) {
+      this.onconnect = onconnect;
+      this.onerror = onerror;
+      this.ontimeout = ontimeout;
+      this.username_sent = false;
+      this.password_sent = false;
+      var _this = this;
+      this.stream.connect(this.params, function (err) {
+        onconnect(new Error(err));
+      });
+      this.stream.setReportErrorHandler(function (telnet, error) {
+        if (onerror) {
+          if (!error) {
+            error = "Connection lost";
+          }
+          onerror(new Error(error));
+        }
+      });
+
+      var timeout_triggered = false;
+      this.stream.setReportTimeoutHandler(function (telnet, error) {
+        if (ontimeout) {
+          if (!timeout_triggered) {
+            timeout_triggered = true;
+            ontimeout(error);
+          }
+        }
+      });
+
+      this.stream.setReportAYTHandler(function (telnetcli, type) {
+        _this.ayt_pending = false;
+      });
+    }
+  }, {
+    key: 'disconnect',
+    value: function disconnect() {
+      this.stream.close();
+    }
+  }, {
+    key: 'registerListener',
+    value: function registerListener(cb) {
+      var _this = this;
+      this.onmessage = cb;
+
+      this.stream.read(function (err, recv) {
+        if (recv) {
+          recv = recv.join('');
+          cb(recv);
+        }
+      });
+    }
+  }, {
+    key: 'send',
+    value: function send(mssg, cb) {
+      var data = new Buffer(mssg, "binary");
+      this.send_raw(data, cb);
+    }
+  }, {
+    key: 'send_raw',
+    value: function send_raw(data, cb) {
+      this.stream.write(data, function () {
+        if (cb) cb();
+      });
+    }
+  }, {
+    key: 'send_cmd',
+    value: function send_cmd(cmd, cb) {
+      var mssg = '\x1b\x1b' + cmd;
+      data = new Buffer(mssg, "binary");
+      this.send_raw(data, cb);
+    }
+  }, {
+    key: 'flush',
+    value: function flush(cb) {
+      cb();
+    }
+  }]);
+
+  return PyTelnet;
+}();
+
+exports.default = PyTelnet;
