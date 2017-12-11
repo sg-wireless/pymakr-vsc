04ddd30589ed8feaf788e3b5c744f447e1835b43
Ralph Hogenbirk <ralph@pycom.io>
2017-12-11 16:42:49 +0100

    Added new precompiles

---
+++ precompiles/1.15/serialport-linux/build/Release/.deps/Release/obj.target/serialport/src/serialport.o.d
@@ -0,0 +1,58 @@
+cmd_Release/obj.target/serialport/src/serialport.o := g++ '-DNODE_GYP_MODULE_NAME=serialport' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/include/node -I/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src -I/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include -I/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/v8/include -I../../nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/serialport/src/serialport.o.d.raw   -c -o Release/obj.target/serialport/src/serialport.o ../src/serialport.cpp
+Release/obj.target/serialport/src/serialport.o: ../src/serialport.cpp \
+ ../src/./serialport.h ../../nan/nan.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node_version.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-errno.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-version.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-unix.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-threadpool.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-linux.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/v8/include/v8.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/v8/include/v8-version.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/v8/include/v8config.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node_version.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node_buffer.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node.h \
+ /home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node_object_wrap.h \
+ ../../nan/nan_callbacks.h ../../nan/nan_callbacks_12_inl.h \
+ ../../nan/nan_maybe_43_inl.h ../../nan/nan_converters.h \
+ ../../nan/nan_converters_43_inl.h ../../nan/nan_new.h \
+ ../../nan/nan_implementation_12_inl.h ../../nan/nan_persistent_12_inl.h \
+ ../../nan/nan_weak.h ../../nan/nan_object_wrap.h ../../nan/nan_private.h \
+ ../../nan/nan_typedarray_contents.h ../../nan/nan_json.h \
+ ../src/./serialport_poller.h ../src/././serialport.h
+../src/serialport.cpp:
+../src/./serialport.h:
+../../nan/nan.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node_version.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-errno.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-version.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-unix.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-threadpool.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/uv/include/uv-linux.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/v8/include/v8.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/v8/include/v8-version.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/deps/v8/include/v8config.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node_version.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node_buffer.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node.h:
+/home/ralph/.electron-gyp/.node-gyp/iojs-1.6.11/src/node_object_wrap.h:
+../../nan/nan_callbacks.h:
+../../nan/nan_callbacks_12_inl.h:
+../../nan/nan_maybe_43_inl.h:
+../../nan/nan_converters.h:
+../../nan/nan_converters_43_inl.h:
+../../nan/nan_new.h:
+../../nan/nan_implementation_12_inl.h:
+../../nan/nan_persistent_12_inl.h:
+../../nan/nan_weak.h:
+../../nan/nan_object_wrap.h:
+../../nan/nan_private.h:
+../../nan/nan_typedarray_contents.h:
+../../nan/nan_json.h:
+../src/./serialport_poller.h:
+../src/././serialport.h:
