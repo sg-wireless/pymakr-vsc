6665dcafb25ae790d625e46b007ae7c5900be00d
Jos Verlinde[MSFT] <jos_verlinde@hotmail.com>
2018-10-03 22:56:41 +0200

    Add default in project settings for reboot_after_upload

--- src/main/settings-wrapper.js
+++ src/main/settings-wrapper.js
@@ -298,6 +298,7 @@ export default class SettingsWrapper extends EventEmitter {
         "sync_folder": this.api.config('sync_folder'),
         "open_on_start": this.api.config('open_on_start'),
         "safe_boot_on_upload": this.api.config('safe_boot_on_upload'),
+        "reboot_after_upload": this.api.config('reboot_after_upload'),
         "statusbar_buttons": this.api.config('statusbar_buttons')
     }
     if(global){
