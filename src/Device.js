c6e9d6e838572b588553aa0870e4eb43551c82ac
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-01-18 14:16:57 +0100

    update Device

--- src/Device.js
+++ src/Device.js
@@ -1,12 +1,19 @@
+const { PySerial } = require("./interfaces/PySerial");
+
 class Device {
   /**
    * @param {import('serialport').PortInfo & {friendlyName?: string}} info
+   * @param {PyMakr} pyMakr
    */
   constructor(info, pyMakr) {
     this.info = info;
     this.name = info.friendlyName;
-    this.pyMakr = pyMakr
-    console.log(this);
+    this.pyMakr = pyMakr;
+    this.log = pyMakr.log.createChild(info.friendlyName);
+    
+    this.interfaces = {
+      usb: new PySerial(this, { address: info.path }),
+    };
   }
 }
 
