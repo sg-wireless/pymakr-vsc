6bc4692687b9531e1329a03aedaf626d3ff106e6
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-03-10 11:17:52 +0100

    test: add stress tests

---
+++ test/suite/integration/stress.test.js
@@ -0,0 +1,21 @@
+const { disconnectAllDevices } = require("./utils");
+test("stress test", async ({ test }) => {
+  const device = pymakr.devicesStore.get()[0];
+  await disconnectAllDevices();
+
+  test("5x (re)connect", async () => {
+    let error;
+    try {
+      const count = 5;
+      for (let i = 0; i <= count; i++) {
+        await device.connect();
+        assert(device.connected);
+        await device.disconnect();
+        assert(!device.connected);
+      }
+    } catch (err) {
+      error = err;
+    }
+    assert(!error, error);
+  });
+});
