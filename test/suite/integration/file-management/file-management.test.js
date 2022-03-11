2d0c7a32fd33aa41eaee7ab8437cbd57001d454b
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-03-11 12:46:55 +0100

    test: upload + erase integration tests

---
+++ test/suite/integration/file-management/file-management.test.js
@@ -0,0 +1,37 @@
+const vscode = require("vscode");
+
+test("file management", async ({ test }) => {
+  const device = pymakr.devicesStore.get()[0];
+  test("can clear device", async () => {
+    await device.connect();
+    await device.adapter.remove("/flash", true);
+    const files = await device.adapter.listFiles("/flash", { recursive: false });
+    assert.deepEqual(files, []);
+  });
+  test("can upload a file", async () => {
+    const uri = vscode.Uri.file(__dirname + "/sample/sample-file-1.md");
+    await pymakr.commands["pymakr.upload"](uri, device, "/sample-file-1.md");
+    const files = await device.adapter.listFiles("/flash", { recursive: false });
+    assert.equal(files.length, 1);
+    assert.equal(files[0].filename, "/flash/sample-file-1.md");
+  });
+  test("can upload a dir", async () => {
+    const uri = vscode.Uri.file(__dirname + "/sample");
+    await pymakr.commands["pymakr.upload"](uri, device, "/");
+    const files = await device.adapter.listFiles("/flash", { recursive: false });
+    assert.equal(files.length, 3);
+    assert.deepEqual(
+      files.map((f) => f.filename),
+      ["/flash/main.py", "/flash/sample-file-1.md", "/flash/sample-file-2.md"]
+    );
+  });
+  test("can erase and provision a device", async () => {
+    await pymakr.commands["pymakr.eraseDevice"]({ device }, "empty");
+    const files = await device.adapter.listFiles("/flash", { recursive: false });
+    assert.equal(files.length, 2);
+    assert.deepEqual(
+      files.map((f) => f.filename),
+      ["/flash/boot.py", "/flash/main.py"]
+    );
+  });
+});
