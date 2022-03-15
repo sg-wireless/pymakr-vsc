8a17d5d819aa781478fdd97eab7f746a1b46197a
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-03-15 08:49:23 +0100

    test: fix missing reports

--- test/suite/index.js
+++ test/suite/index.js
@@ -1,7 +1,7 @@
 async function run() {
   const { probs } = await import("probs");
 
-  await probs([__dirname + "/integration"], { runner: "main", concurrency: 1 });
+  await probs([__dirname + "/integration"], { runner: "main", concurrency: 1, reporter: 'consoleReporter' });
 }
 
 module.exports = {
