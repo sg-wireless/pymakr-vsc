1cdc96672e48dd408594a2bd877dcc5bc325732c
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-03-28 11:02:09 +0200

    refactor: remove deprecated command

--- src/commands/index.js
+++ src/commands/index.js
@@ -337,48 +337,6 @@ class Commands {
 
     newDeviceRecover: async () => {},
 
-    newDevice: async () => {
-      const { label: protocol } = await vscode.window.showQuickPick([
-        {
-          label: "telnet",
-          description: "Network device",
-        },
-        {
-          label: "serial",
-          description: "USB device",
-        },
-        {
-          label: "Recover device",
-          description: "Recover a previously hidden device",
-        },
-      ]);
-
-      const isTelnet = protocol === "telnet";
-
-      const placeHolder = isTelnet ? "192.168.0.x" : process.platform === "win32" ? "COM3" : "/dev/tty-usbserial3";
-      const prompt = protocol === "telnet" ? "Hostname or IP of your device" : "Path to your device";
-      const address = await vscode.window.showInputBox({ placeHolder, prompt });
-
-      const username = isTelnet
-        ? await vscode.window.showInputBox({ prompt: "Username for your device [default: micro]", value: " micro" })
-        : "";
-
-      const password = isTelnet
-        ? await vscode.window.showInputBox({
-            password: true,
-            prompt: "Password for your device [default: python]",
-            value: "python",
-          })
-        : "";
-
-      const name = await vscode.window.showInputBox({
-        value: `${protocol}://${address}`,
-        prompt: "Name of your device",
-      });
-
-      this.pymakr.devicesStore.upsert({ address, protocol, name, username, password });
-    },
-
     /**
      * @param {ProjectDeviceTreeItem} treeItem
      */
