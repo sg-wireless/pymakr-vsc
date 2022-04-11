4b4b8a1015234fe6509674193decdd7169d7d788
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-04-11 09:30:14 +0200

    fix: info should log to the output tab

--- src/utils/createLogger.js
+++ src/utils/createLogger.js
@@ -3,33 +3,24 @@ const vscode = require("vscode");
 
 /**
  * Creates Consolite instance to handle all logging
- * @param {string} name 
+ * @param {string} name
  */
 const createLogger = (name) => {
-  //Create output channel
-  let outputChannel = vscode.window.createOutputChannel("PyMakr")
+  const outputChannel = vscode.window.createOutputChannel("PyMakr");
 
-  let outputLogLine = outputChannel.appendLine.bind(outputChannel)
-  let outputLog = outputChannel.append.bind(outputChannel)
-
-  // todo: remove before release as this distracts
-  outputChannel.show()
-
-  //Write to output.
-  outputLogLine("This is a bound logger")
-  outputLog("Part of a message:")
-  outputLogLine(" the remainder of the message")
   const log = _createLogger(
     {
       methods: {
         debugShort: console.log,
         traceShort: console.log,
-        info: outputLogLine, // todo this is not working
+        info: (...str) => {
+          console.log("info: ", ...str); // in case we need a copy/paste of the console
+          outputChannel.appendLine(str.join(" "));
+        },
       },
     },
     name
   );
-
 
   log.levels = {
     fatal: 0,
@@ -44,10 +35,6 @@ const createLogger = (name) => {
     trace: 7,
   };
   log.delimiter = ">";
-
-  log.info("Is this the bound logger ?? ") // todo this is not working either
-
-  outputLogLine("This is a bound logger from outputLogLine")
 
   return log;
 };
