219e7431ad15dd5175dd1a43fb2fe6590598d033
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-04-19 10:22:17 +0200

    chore: stores aren't disposable
    only store subscriptions are disposable

--- extension.js
+++ extension.js
@@ -1,7 +1,5 @@
 const { PyMakr } = require("./src/PyMakr");
 
-/** @type {PyMakr} */
-var thisExtension;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -9,17 +7,12 @@ var thisExtension;
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-  thisExtension = new PyMakr(context);
-
+  new PyMakr(context);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {
   //todo: close any open terminals to avoid them hanging when VScode is restarted at a later stage.
-  thisExtension.terminalsStore.dispose();
-  thisExtension.devicesStore.dispose();
-  thisExtension.projectsStore.dispose();
-
 }
 
 module.exports = {
