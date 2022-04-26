2e3cecc6297118ab6495dc9ecb1d9bae58e9d7c6
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-04-26 08:28:54 +0200

    chore: add example for test watch and pattern use

--- test/suite/runIntegrationTests.js
+++ test/suite/runIntegrationTests.js
@@ -1,6 +1,12 @@
 async function run() {
   const { probs } = await import("probs");
-  await probs([__dirname + "/integration"], { runner: "main", concurrency: 1, reporter: 'consoleReporter'});
+  await probs([__dirname + "/integration"], {
+    runner: "main",
+    concurrency: 1,
+    reporter: "consoleReporter",
+    // watch: true,
+    // pattern: ["busy-devices.test.js // some test // some nested test"],
+  });
 }
 
 module.exports = {
