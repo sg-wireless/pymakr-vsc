ae9ca5bfd221d29c2ace9763b387b695dbd68c4d
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-05-06 14:54:50 +0200

    chore: add sleep helper

--- test/suite/integration/utils.js
+++ test/suite/integration/utils.js
@@ -1,3 +1,4 @@
 const disconnectAllDevices = () => Promise.all(pymakr.devicesStore.get().map((device) => device.disconnect()));
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
-module.exports = { disconnectAllDevices };
+module.exports = { disconnectAllDevices, sleep };
