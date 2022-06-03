4b21622a50c522bf86415f14b6b176720aefe9c2
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-06-03 13:16:08 +0200

    refactor: change how config and state are stored

--- test/suite/integration/file-management/file-management.test.js
+++ test/suite/integration/file-management/file-management.test.js
@@ -8,45 +8,45 @@ test("file management", async () => {
 
   test("can clear device", async () => {
     await device.connect();
-    await device.adapter.remove(device.rootPath, true);
-    const files = await device.adapter.listFiles(device.rootPath, { recursive: false });
+    await device.adapter.remove(device.config.rootPath, true);
+    const files = await device.adapter.listFiles(device.config.rootPath, { recursive: false });
     assert.deepEqual(files, []);
   });
 
   test("can upload a file", async () => {
     const uri = vscode.Uri.file(join(__dirname, "_sample/sample-file-1.md"));
     await pymakr.commands.upload(uri, device, "/sample-file-1.md");
-    const files = await device.adapter.listFiles(device.rootPath, { recursive: false });
+    const files = await device.adapter.listFiles(device.config.rootPath, { recursive: false });
     assert.equal(files.length, 1);
-    assert.equal(files[0].filename, posix.join(device.rootPath, "/sample-file-1.md"));
+    assert.equal(files[0].filename, posix.join(device.config.rootPath, "/sample-file-1.md"));
   });
 
   test("can upload a dir", async () => {
     const uri = vscode.Uri.file(__dirname + "/_sample");
     await pymakr.commands.upload(uri, device, "/");
-    const files = await device.adapter.listFiles(device.rootPath, { recursive: false });
+    const files = await device.adapter.listFiles(device.config.rootPath, { recursive: false });
     assert.deepEqual(
       files.map((f) => f.filename),
       [
-        posix.join(device.rootPath, "includeme"),
-        posix.join(device.rootPath, "main.py"),
-        posix.join(device.rootPath, "pymakr.conf"),
-        posix.join(device.rootPath, "sample-file-1.md"),
-        posix.join(device.rootPath, "sample-file-2.md"),
+        posix.join(device.config.rootPath, "includeme"),
+        posix.join(device.config.rootPath, "main.py"),
+        posix.join(device.config.rootPath, "pymakr.conf"),
+        posix.join(device.config.rootPath, "sample-file-1.md"),
+        posix.join(device.config.rootPath, "sample-file-2.md"),
       ]
     );
   });
 
   test("can erase and provision a device", async () => {
     await pymakr.commands.eraseDevice({ device }, "empty");
-    const files = await device.adapter.listFiles(device.rootPath, { recursive: false });
+    const files = await device.adapter.listFiles(device.config.rootPath, { recursive: false });
     assert.equal(files.length, 3);
     assert.deepEqual(
       files.map((f) => f.filename),
       [
-        posix.join(device.rootPath, "boot.py"),
-        posix.join(device.rootPath, "main.py"),
-        posix.join(device.rootPath, "pymakr.conf"),
+        posix.join(device.config.rootPath, "boot.py"),
+        posix.join(device.config.rootPath, "main.py"),
+        posix.join(device.config.rootPath, "pymakr.conf"),
       ]
     );
   });
