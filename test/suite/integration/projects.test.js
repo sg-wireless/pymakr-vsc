1c63937c3f3d6677471c6e54938eb8e257b46bd7
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-08-16 10:15:08 +0200

    test: empty dist_dir should work

--- test/suite/integration/projects.test.js
+++ test/suite/integration/projects.test.js
@@ -54,3 +54,19 @@ test("can upload project", async () => {
     assert.equal(file.toString(), 'print("hello world")');
   });
 });
+
+test("can change dist_dir to root", () => {
+  const configFile = readFileSync(`${projectPath1}/pymakr.conf`, "utf8");
+  const config = JSON.parse(configFile);
+  config.dist_dir = "";
+  writeFileSync(`${projectPath1}/pymakr.conf`, JSON.stringify(config, null, 2));
+  const device = getDevice();
+  const project = getProject();
+  test('"upload project" uploads the dist_dir', async () => {
+    const root = device.config.rootPath;
+    await pymakr.commands.uploadProject({ device, project });
+    const files = await device.adapter.listFiles(root, { recursive: true });
+    const filenames = files.map((file) => file.filename);
+    assert.deepEqual(filenames, [`${root}`, `${root}/device`, `${root}/device/script.py`, `${root}/pymakr.conf`]);
+  });
+});
